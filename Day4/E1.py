9df250eb5e0830a1b1ddec4b43902bb71ee21b50
KyleForan <Kyleforan127@hotmail.com>
2024-12-04 15:01:04 +0000

    Day 4 E1 Minor Corrections

--- Day4/E1.py
+++ Day4/E1.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 data = open("Day4/input").read()
-data = [ list(line) for line in data.split("\n")]
+data = [ list(line) for line in data.split("\n") ]
 npData = np.array(data)
 
 height, width = len(data), len(data[0])
 
 leftRight = data[:]
-UpDown = list(map(list, zip(*data)))
-downLeft = [ np.diag(npData, x).tolist() for x in range((height)*-1, width)] 
-downRight = [ np.diag(npData[::-1], x).tolist() for x in range((height-4)*-1, width-4)] 
+upDown = list(map(list, zip(*data)))
+downLeft = [ np.diag(npData, x).tolist() for x in range((height)*-1, width) ] 
+downRight = [ np.diag(npData[::-1], x).tolist() for x in range((height)*-1, width) ] 
 
 result = 0
 
-for arr in [UpDown, leftRight, downLeft, downRight]:
+for arr in [upDown, leftRight, downLeft, downRight]:
     for sub in arr:
         result += ''.join(sub).count("XMAS") + ''.join(sub).count("XMAS"[::-1])
 
