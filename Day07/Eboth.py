0d4d8ef0cb4b8ffde221e9cba46a11cfafa44e97
KyleForan <Kyleforan127@hotmail.com>
2024-12-07 15:12:20 +0000

    Day07 Prints both solutions

--- Day07/Eboth.py
+++ Day07/Eboth.py
@@ -1,5 +1,5 @@
 data = open('Day07/input').read().split('\n')
-total = 0
+total = [0, 0]
 
 for line in data:
     if not line: continue
@@ -8,17 +8,20 @@ for line in data:
     result = int(result)
     nums = [int(num) for num in nums.split(' ')]
 
-    possibilities = [nums.pop(0)]
+    possibilities = [(nums.pop(0), 1)]
 
     for num in nums:
         tempPossibilities = []
-        for p in possibilities:
-            # Part 2 is this line ↓
-            # tempPossibilities.append(int(str(p) + str(num))) 
-            tempPossibilities.append(p + num)
-            tempPossibilities.append(p * num)
+        for possible, part in possibilities:
+            tempPossibilities.append((int(str(possible) + str(num)), 2)) 
+            tempPossibilities.append((possible + num, part))
+            tempPossibilities.append((possible * num, part))
         possibilities = tempPossibilities
 
-    if result in possibilities: total += result
+    part1 = result in [el[0] for el in possibilities if el[1] == 1]
+    part2 = result in [el[0] for el in possibilities]
+    
+    total = [total[0] + (result * part1), total[1] + (result * part2)]
 
-print(total)
+print(f"Part1: {total[0]}")
+print(f"Part2: {total[1]}")
